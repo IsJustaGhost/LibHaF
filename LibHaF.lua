c895676eeb36eb5b357dd0d4fffa1f9f9bcf4326
IsJustaGhost <56104601+IsJustaGhost@users.noreply.github.com>
2022-03-17 01:08:32 -0700

    Added JO_CallLater functions

--- LibHaF.lua
+++ LibHaF.lua
@@ -65,7 +65,7 @@ _G[LIB_IDENTIFIER] = lib
 ]]
 
 local debugOverride = true
-local logLevel = debugOverride and LibDebugLogger.LOG_LEVEL_DEBUG or LibDebugLogger.LOG_LEVEL_INFO
+local logLevel
 
 local function unpack_unordered_recursive(tbl, key)
   local new_key, value = next(tbl, key)
@@ -589,6 +589,69 @@ function ZO_Object:UnregisterForPostHook(...)
 	return HOOK_MANAGER:UnregisterForPostHook(tostring(self), ...)
 end
 ]]
+
+
+---------------------------------------------------------------------------------------------------------------
+-- JO_CallLater and JO_CallLaterOnScene
+---------------------------------------------------------------------------------------------------------------
+--[[ About:
+	These functions will not stack the callback if called several times in a row.
+
+	JO_CallLater is simmular to zo_callLater, but can be ran several times in a row without stacking.
+
+	JO_CallLaterOnScene is useful for situations where you want changes applied after a specified scene is shown.
+
+	JO_CallLaterOnNextScene is useful for situations where you want changes applied after scenes change.
+
+
+	JO_CallLater('_test', function() end, 100)
+	JO_CallLaterOnScene(addon.name .. '_test', 'hud', function() end)
+	JO_CallLaterOnNextScene(addon.name .. '_test', function() end)
+]]
+
+JO_CallLater = function(id, func, ms)
+	if ms == nil then ms = 0 end
+    local name = "JO_CallLater_".. id
+	EVENT_MANAGER:UnregisterForUpdate(name)
+	
+    EVENT_MANAGER:RegisterForUpdate(name, ms,
+        function()
+            EVENT_MANAGER:UnregisterForUpdate(name)
+            func(id)
+        end)
+    return id
+end
+
+JO_CallLaterOnScene = function(id, sceneName, func)
+	if not sceneName or type(sceneName) ~= 'string' then return end
+	
+	local updateName = "JO_CallLaterOnScene_" .. id
+    EVENT_MANAGER:UnregisterForUpdate(updateName)
+	
+	local function OnUpdateHandler()
+		if SCENE_MANAGER:GetCurrentSceneName() == sceneName then
+			EVENT_MANAGER:UnregisterForUpdate(updateName)
+			func()
+		end
+	end
+	
+	EVENT_MANAGER:RegisterForUpdate(updateName, 100, OnUpdateHandler)
+end
+
+JO_CallLaterOnNextScene = function(id, func)
+	local sceneName = SCENE_MANAGER:GetCurrentSceneName()
+	local updateName = "JO_CallLaterOnNextScene_" .. id
+    EVENT_MANAGER:UnregisterForUpdate(updateName)
+	
+	local function OnUpdateHandler()
+		if SCENE_MANAGER:GetCurrentSceneName() ~= sceneName then
+			EVENT_MANAGER:UnregisterForUpdate(updateName)
+			func()
+		end
+	end
+	
+	EVENT_MANAGER:RegisterForUpdate(updateName, 100, OnUpdateHandler)
+end
 
 ---------------------------------------------------------------------------------------------------------------
 -- DeferredInitialization - functions that need to be set up after loading
